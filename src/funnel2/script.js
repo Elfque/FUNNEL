7d265d75c2dda7bd0778233e0d96e50947d050bc
Elfque <100863540+Elfque@users.noreply.github.com>
2024-03-07 20:57:10 +0100

    funnel2 done

---
+++ src/funnel2/script.js
@@ -0,0 +1,44 @@
+const menuBtn = document.querySelector(".menu-button");
+const menuMain = document.querySelector(".menu-md");
+
+menuBtn.addEventListener("click", () => {
+  console.log("ksk");
+  menuMain.classList.toggle("active");
+});
+
+const tit = document.querySelectorAll(".tit");
+
+tit.forEach((item) => {
+  item.addEventListener("click", () => {
+    const parent = item.parentElement;
+
+    const answer = parent.querySelector(".detail");
+    item.classList.toggle("bg-blue-300");
+    answer.classList.toggle("active");
+  });
+});
+
+const modal = document.querySelector(".modal");
+const btnOpen = document.querySelectorAll(".open-btn");
+const overlay = document.querySelector(".overlay");
+const closeBtn = document.querySelector(".cancel-icon");
+
+btnOpen.forEach((item) => {
+  item.addEventListener("click", () => {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+});
+
+const closeModal = () => {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+overlay.addEventListener("click", closeModal);
+closeBtn.addEventListener("click", closeModal);
+
+const directToPayBtn = document.querySelector(".go-pay");
+
+directToPayBtn.addEventListener("click", () => {
+  window.location.replace("/src/funnel2/order.html");
+});
